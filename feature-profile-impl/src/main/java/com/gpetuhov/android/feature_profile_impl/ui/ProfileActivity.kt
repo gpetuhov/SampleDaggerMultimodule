291f6aaa01820d17f785d6ae20292c2a78cc48f4
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-28 16:35:29 +0300

    Inject dependencies into ProfileActivity

--- feature-profile-impl/src/main/java/com/gpetuhov/android/feature_profile_impl/ui/ProfileActivity.kt
+++ feature-profile-impl/src/main/java/com/gpetuhov/android/feature_profile_impl/ui/ProfileActivity.kt
@@ -2,15 +2,37 @@ package com.gpetuhov.android.feature_profile_impl.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gpetuhov.android.core_db_api.data.DbClientApi
+import com.gpetuhov.android.core_utils.utils.Utils
 import com.gpetuhov.android.feature_profile_impl.R
+import com.gpetuhov.android.feature_profile_impl.di.components.ProfileFeatureComponent
+import kotlinx.android.synthetic.main.activity_profile.*
+import javax.inject.Inject
 
 class ProfileActivity : AppCompatActivity() {
+
+    // ProfileActivity and ProfileFeatureComponent are inside the same module,
+    // so they know of each other, and we can inject dependencies as usual.
+    @Inject lateinit var utils: Utils
+    @Inject lateinit var dbClientApi: DbClientApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        // TODO: get app title and user name from external dependencies
+        // Inject dependencies and use them
+        ProfileFeatureComponent.get()?.inject(this)
+        app_title.text = utils.getAppTitle()
+        user_name.text = dbClientApi.getUser().name
     }
 
+    override fun onPause() {
+        if (isFinishing) {
+            // Clear component if the activity is finishing.
+            // It is better to do it like this,
+            // because onDestroy() is not guaranteed to be called.
+            ProfileFeatureComponent.resetComponent()
+        }
+        super.onPause()
+    }
 }
