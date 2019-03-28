4c36e356a17af02ede04f186e4dd0e29585796d0
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-28 17:49:43 +0300

    Add feature-first-api module

---
+++ feature-first-api/src/main/java/com/gpetuhov/android/feature_first_api/FirstFeatureApi.kt
@@ -0,0 +1,5 @@
+package com.gpetuhov.android.feature_first_api
+
+interface FirstFeatureApi {
+    fun firstFeatureStarter(): FirstFeatureStarter
+}
---
+++ feature-first-api/src/main/java/com/gpetuhov/android/feature_first_api/FirstFeatureStarter.kt
@@ -0,0 +1,7 @@
+package com.gpetuhov.android.feature_first_api
+
+import android.content.Context
+
+interface FirstFeatureStarter {
+    fun start(context: Context)
+}
