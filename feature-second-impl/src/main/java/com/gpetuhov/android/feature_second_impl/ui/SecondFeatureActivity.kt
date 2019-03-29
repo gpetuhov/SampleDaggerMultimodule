e0a72bb512bdfed06ed9e1cf74a001314e012d3c
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-29 10:35:02 +0300

    Add feature-second-impl module

---
+++ feature-second-impl/src/main/java/com/gpetuhov/android/feature_second_impl/ui/SecondFeatureActivity.kt
@@ -0,0 +1,13 @@
+package com.gpetuhov.android.feature_second_impl.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gpetuhov.android.feature_second_impl.R
+
+class SecondFeatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_feature)
+    }
+}
