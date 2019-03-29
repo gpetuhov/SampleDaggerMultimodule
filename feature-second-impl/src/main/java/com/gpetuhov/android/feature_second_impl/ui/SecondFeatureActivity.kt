27154e05ccfed641df51eb64cbe84a900fbd09cc
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-29 11:09:28 +0300

    Inject dependencies into SecondFeatureActivity

--- feature-second-impl/src/main/java/com/gpetuhov/android/feature_second_impl/ui/SecondFeatureActivity.kt
+++ feature-second-impl/src/main/java/com/gpetuhov/android/feature_second_impl/ui/SecondFeatureActivity.kt
@@ -2,12 +2,31 @@ package com.gpetuhov.android.feature_second_impl.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gpetuhov.android.core_utils.utils.Utils
+import com.gpetuhov.android.feature_profile_api.ProfileFeatureStarter
 import com.gpetuhov.android.feature_second_impl.R
+import com.gpetuhov.android.feature_second_impl.di.components.SecondFeatureComponent
+import kotlinx.android.synthetic.main.activity_second_feature.*
+import javax.inject.Inject
 
 class SecondFeatureActivity : AppCompatActivity() {
+
+    @Inject lateinit var utils: Utils
+    @Inject lateinit var profileFeatureStarter: ProfileFeatureStarter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_feature)
+
+        SecondFeatureComponent.get()?.inject(this)
+        app_title.text = utils.getAppTitle()
+        profile_button.setOnClickListener { profileFeatureStarter.start(this) }
+    }
+
+    override fun onPause() {
+        if (isFinishing) {
+            SecondFeatureComponent.resetComponent()
+        }
+        super.onPause()
     }
 }
